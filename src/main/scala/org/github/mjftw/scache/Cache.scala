3851bf05ea94e799786a84fb8978f74092d42a86
Merlin Webster <mjftwebster@gmail.com>
2021-06-24 23:57:58 +0100

    Combined cache types

--- src/main/scala/org/github/mjftw/scache/Cache.scala
+++ src/main/scala/org/github/mjftw/scache/Cache.scala
@@ -13,40 +13,45 @@ import cats.effect.Timer
 
 trait Cache[F[_], A, B] {
   def put(key: A, value: B): F[Unit]
+  def putWithExpiry(key: A, value: B, expireAfter: FiniteDuration): F[Unit]
   def get(key: A): F[Option[B]]
 }
 
 object Cache {
-  /** Create a simple cache */
-  def make[F[_]: Sync, A, B]: F[Cache[F, A, B]] =
-    Ref.of[F, Map[A, B]](Map.empty[A, B]).map { ref =>
-      new Cache[F, A, B] {
-        def put(key: A, value: B) = ref.update(_ + (key -> value))
-        def get(key: A): F[Option[B]] = ref.get.map(_.get(key))
-      }
-    }
 
   /** Create a cache whose entries are invalidated after an amount of time */
-  def makeExpiring[F[_]: Concurrent: Timer, A, B](keyExpiry: FiniteDuration): F[Cache[F, A, B]] =
-    Ref.of[F, Map[A, (B, Fiber[F, Unit])]](Map.empty[A, (B, Fiber[F, Unit])]).map { ref =>
-      new Cache[F, A, B] {
-        def put(key: A, value: B) =
-          for {
-            // Cancel any ongoing removal timer
-            _ <- ref.get.map(_.get(key) match {
-              case Some((_, fiber)) => fiber.cancel
-              case None => Sync[F].unit
-            }).flatten
-            // Start a new removal timer
-            fiber <- Concurrent[F].start {
-              Timer[F].sleep(keyExpiry) *> remove(key)
+  def make[F[_]: Concurrent: Timer, A, B]: F[Cache[F, A, B]] =
+    Ref.of[F, Map[A, (B, Option[Fiber[F, Unit]])]](Map.empty[A, (B, Option[Fiber[F, Unit]])]).map {
+      ref =>
+        new Cache[F, A, B] {
+          def put(key: A, value: B) =
+            for {
+              _ <- cancelRemoval(key)
+              _ <- ref.update(_ + (key -> Tuple2(value, None)))
+            } yield ()
+
+          def putWithExpiry(key: A, value: B, expireAfter: FiniteDuration) =
+            for {
+              _ <- cancelRemoval(key)
+              removalTimer <- scheduleRemoval(key, expireAfter)
+              _ <- ref.update(_ + (key -> Tuple2(value, Some(removalTimer))))
+            } yield ()
+
+          def get(key: A): F[Option[B]] = ref.get.map(_.get(key).map { case (value, _) => value })
+
+          /** Schedule removal removal of a key */
+          private def scheduleRemoval(key: A, removeAfter: FiniteDuration): F[Fiber[F, Unit]] =
+            Concurrent[F].start {
+              Timer[F].sleep(removeAfter) *> ref.update(_ - key)
             }
-            _ <- ref.update(_ + (key -> Tuple2(value, fiber)))
-        } yield ()
 
-        def get(key: A): F[Option[B]] = ref.get.map(_.get(key).map{case (value, _) => value})
-
-        private def remove(key: A): F[Unit] = ref.update(_ - key)
-      }
+          /** Cancel any ongoing removal timer */
+          private def cancelRemoval(key: A): F[Unit] = ref.get
+            .map(_.get(key) match {
+              case Some((_, Some(fiber))) => fiber.cancel
+              case _                      => Sync[F].unit
+            })
+            .flatten
+        }
     }
 }
