3c339fbc707f5cc3e96e3276151cc67ae62be661
Merlin Webster <mjftwebster@gmail.com>
2021-06-24 23:24:56 +0100

    Added a cache that expires entries

--- src/main/scala/org/github/mjftw/scache/Cache.scala
+++ src/main/scala/org/github/mjftw/scache/Cache.scala
@@ -3,6 +3,13 @@ package org.github.mjftw.scache
 import cats.effect.concurrent.Ref
 import cats.effect.Sync
 import cats.implicits._
+import scala.concurrent.duration.FiniteDuration
+import cats.effect.Fiber
+import cats.effect.Async
+import cats.effect.Concurrent
+import cats.effect.IO
+import cats.effect.LiftIO
+import cats.effect.Timer
 
 trait Cache[F[_], A, B] {
   def put(key: A, value: B): F[Unit]
@@ -15,6 +22,35 @@ object Cache {
       new Cache[F, A, B] {
         def put(key: A, value: B) = ref.update(_ + (key -> value))
         def get(key: A): F[Option[B]] = ref.get.map(_.get(key))
+      }
+    }
+
+  def makeExpiring[F[_]: Concurrent: Timer, A, B](keyExpiry: FiniteDuration): F[Cache[F, A, B]] =
+    Ref.of[F, Map[A, (B, Fiber[F, Unit])]](Map.empty[A, (B, Fiber[F, Unit])]).map { ref =>
+      new Cache[F, A, B] {
+        def put(key: A, value: B) =
+          for {
+            // Cancel any ongoing removal timer
+            _ <- ref.get.map(_.get(key) match {
+              case Some((_, fiber)) => fiber.cancel
+              case None => Sync[F].unit
+            }).flatten
+            // Start a new removal timer
+            fiber <- Concurrent[F].start {
+              Timer[F].sleep(keyExpiry) *> remove(key)
+            }
+            _ <- ref.update(_ + (key -> Tuple2(value, fiber)))
+        } yield ()
+
+        def get(key: A): F[Option[B]] = ref.get.map(_.get(key).map{case (value, _) => value})
+
+        private def remove(key: A): F[Unit] = ref.update(_ - key)
+
+        private def delay[A](duration: FiniteDuration)(block: => A): F[A] =
+          for {
+            _ <- Timer[F].sleep(duration)
+            a <- Sync[F].delay(block)
+          } yield a
       }
     }
 }
